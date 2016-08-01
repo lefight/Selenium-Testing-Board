458235f192439c7133f7ca61e18becdf39caf940
lefight <torsten@t-hobein.de>
2016-08-01 18:08:25 +0200

    TestSummary
    Test

---
+++ src/tools/Commands.java
@@ -0,0 +1,24 @@
+package tools;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.internal.Configuration;
+
+
+public class Commands {
+/**
+    public static void clickAndWaitForPageToLoad(By locator) {
+        clickAndWaitForPageToLoad(locator, Configuration.getDefaultTimeout());
+    }
+
+    public static void clickAndWaitForPageToLoad(WebElement element) {
+        click(element);
+        waitForPageToLoad(Configuration.getDefaultTimeout());
+    }
+
+    public static void clickAndWaitForPageToLoad(By locator, Timeout timeout) {
+        click(locator);
+        waitForPageToLoad(timeout, locator);
+    }
+**/
+}
