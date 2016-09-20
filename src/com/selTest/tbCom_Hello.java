224f7fae51bcea3a12681ace97ca65a540447098
lefight <torsten@t-hobein.de>
2016-09-20 14:16:14 +0200

    switch frame

--- src/com/selTest/tbCom_Hello.java
+++ src/com/selTest/tbCom_Hello.java
@@ -1,12 +1,10 @@
 package com.selTest;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Proxy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.internal.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -35,12 +33,17 @@ public class tbCom_Hello {
 
         driver.findElement(By.id("user_pass")).submit();
 
+
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return(true);
+            }
+        });
+
 /**
         WebElement meMenu = driver.findElement(By.cssSelector("img.avatar.avatar-32"));
         Actions action = new Actions(driver);
-
         action.moveToElement(meMenu).build().perform();
-
         driver.findElement(By.xpath("//span[2]/a[2]")).click();
 **/
 
@@ -51,25 +54,33 @@ public class tbCom_Hello {
         driver.findElement(By.id("content-html")).click();
         driver.findElement(By.id("content")).sendKeys("Dies ist mein erster Text mit Selenium");
 
+        // click on "Kontaktformular hinzufügen"
         driver.findElement(By.id("insert-jetpack-contact-form")).click();
 
-
-
-        //System.out.println();
-
+        // switch to the next frame
         driver.switchTo().frame("TB_iframeContent");
 
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return(false);
+            }
+        });
+
+        // select name field and enter text
         driver.findElement(By.id("fb-fieldname")).sendKeys("ToHo");
 
+        System.out.println("leave Kontaktformular");
+
+        //driver.switchTo().defaultContent();
+        //Alert alert = driver.switchTo().alert();
+        //click on "Dateien hinzufügen"
+        //insert-media-button
 
 
 
         System.out.println("RunEnde");
 
-
-
-
-
+        //driver.close();
 
 
 
